a4fa64b2abe43eb8eadfcb98707d3cdeed9f0557
yowcow <yowcow@cpan.org>
2017-06-08 18:53:06 +0900

    build executable

--- Makefile
+++ Makefile
@@ -1,9 +1,20 @@
 .PHONY: all test
 
-all: Godeps
+BINARY = go-webapp
+
+all: Godeps $(BINARY)
 
 Godeps:
 	godep save
 
 test:
 	GIN_MODE=release go test -v
+
+$(BINARY):
+	go build -o $@
+
+run:
+	./$(BINARY)
+
+clean:
+	rm -rf $(BINARY)
--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/yowcow/go-webapp/action"
 	"gopkg.in/gin-gonic/gin.v1"
 )
@@ -9,4 +10,18 @@ func Build(app *gin.Engine) {
 	app.GET("/", action.HandleRoot)
 	app.POST("/json", action.HandleJsonBody)
 	app.POST("/form", action.HandleFormBody)
+}
+
+var (
+	port string
+)
+
+func main() {
+	flag.StringVar(&port, "port", "8888", "Port to listen to")
+	flag.Parse()
+
+	router := gin.Default()
+	Build(router)
+
+	router.Run(":" + port)
 }
